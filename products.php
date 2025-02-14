dceedcc557b28ad672f2281b6f611e131c885415
Ekaterina <ekaterina-v-04@mail.ru>
2025-02-14 10:58:30 +0700

    добавила qr

--- products.php
+++ products.php
@@ -14,7 +14,14 @@ require_once 'api/auth/AuthCheck.php';
 AuthCheck('', 'login.php');
 require_once 'api/helpers/InputDefaultValue.php';
 
-
+require 'vendor/autoload.php';
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+$qrCode = new QrCode('Hello, World!');
+$writer = new PngWriter();
+$result = $writer->write($qrCode);
+header('Content-Type: '.$result->getMimeType());
+echo $result->getString();
 
 
 
