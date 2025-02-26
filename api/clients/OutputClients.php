bc3a2f761b98aeed52d54b1b9febbc7e943e5ae0
Ekaterina <ekaterina-v-04@mail.ru>
2025-02-26 11:23:30 +0700

    дата и пдф

--- api/clients/OutputClients.php
+++ api/clients/OutputClients.php
@@ -9,7 +9,26 @@ function OutputClients($clients){
                 <td>{$client['phone']}</td>
                 <td>{$client['birthday']}</td>
                 <td>{$client['created_at']}</td>
-                <td onclick=\"MicroModal.show('history-modal')\"><i class=\"fa fa-history\" aria-hidden=\"true\"></i></td>
+                <td>
+                    <form class=\"date-form\" 
+                          action=\"api/clients/ClientHistory.php\" 
+                          method=\"GET\">
+                        <input type=\"hidden\" 
+                               name=\"id\" 
+                               value=\"{$client['id']}\">
+                        <input type=\"date\" 
+                               id=\"from-{$client['id']}\" 
+                               name=\"from\" 
+                               class=\"date-input\">
+                        <input type=\"date\" 
+                               id=\"to-{$client['id']}\" 
+                               name=\"to\" 
+                               class=\"date-input\">
+                        <button type=\"submit\" class=\"btn\">
+                            Сформировать
+                        </button>
+                    </form>
+                </td>
                 <td onclick=\"MicroModal.show('edit-modal')\"><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i></td>
                 <td>
                 <a href='api/clients/DeleteClient.php?id={$client['id']}'>
